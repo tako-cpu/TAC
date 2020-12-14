b861385c77cd3843180ccdbed6c2a1b0a434673a
John Damian <raxoc52383@idcbill.com>
2020-12-14 10:42:18 +0800

    Added conversion of seconds to hours, Added prompts

--- calculator.sh
+++ calculator.sh
@@ -13,7 +13,8 @@ if [ "$#" -ne 0 ]
 then
 	echo "Your file to be opened: '$1'"
 	SUM=$(awk '{SUM+=$1} END {print SUM}' "$1")
-	echo $SUM
+	echo "Your Total Hours: $(( SUM / 3600 ))"
+	echo "Your Total Seconds: ${SUM}"
 	exit
 fi
 
@@ -21,19 +22,22 @@ fi
 echo "=========================== Time Calculator ==========================="
 read -p "Enter Title: " _TITLE
 FILE="stash/$(whoami)_${_TITLE}_$(date +'%m-%d-%y_%T').txt"
+COUNTER=0
 while true
 do
-	read -p "Enter Hours: " _HOURS
+	COUNTER=$(( COUNTER + 1 ))
+	echo "#${COUNTER}:"
+	read -p "     Enter Hours (Enter '-1' to exit): " _HOURS
 	if [ "${_HOURS}" -lt 0 ]
 	then
 		break
 	fi
-	read -p "Enter Minutes: " _MINUTES
+	read -p "     Enter Minutes (Enter '-1' to exit): " _MINUTES
 	if [ "${_MINUTES}" -lt 0 ]
 	then
 		break
 	fi
-	read -p "Enter Seconds: " _SECONDS
+	read -p "     Enter Seconds (Enter '-1' to exit): " _SECONDS
 	if [ "${_SECONDS}" -lt 0 ]
 	then
 		break
